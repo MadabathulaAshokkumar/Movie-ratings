66471f2515826408e29e5cf5e6c4840ae96d0773
Ashok <ak8341957972@gmail.com>
2023-11-03 20:43:19 +0530

    second commit

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=f89249349d8ec692593d42a690cfe2f4&query=';
 
 const main = document.querySelector(".main");
-const search = document.getElementById("search");
+const search = document.querySelector(".search");
 const searchBtn = document.querySelector('.searchBtn');
-const form = document.getElementsByTagName("form");
+const form = document.querySelector(".search_form");
 getMovieDetails(API_URL);
 
 async function getMovieDetails(url){
@@ -30,7 +30,7 @@ function showMovies(movies){
        <img src="${IMG_PATH + poster}" alt="${title}">
             <div class="movie-info">
                 <h3>${title}</h3>
-             <span class="${chooseColor(voteAvg)}">${voteAvg}</span>
+             <span class="${chooseColor(voteAvg)}">${voteCal(voteAvg)}</span>
             </div>
             <div class="overview">
                 <h3>overview</h3>
@@ -38,19 +38,17 @@ function showMovies(movies){
             </div>`
        main.appendChild(movieEl);
    });
-   console.log(movies);
-
+//    console.log(movies);
 }
 
 searchBtn.addEventListener('click', (e)=>{
     e.preventDefault();
     const searchTerm = search.value;
-    if(searchTerm !== ""){
-        console.log(searchTerm);
-        showMovies(SEARCH_API + searchTerm);
-        search.value = "";
+    if(searchTerm  &&  searchTerm !== ""){
+        getMovieDetails(SEARCH_API + searchTerm);
+        search.value ='';
     }else{
-        console.log("no");
+       window.location.reload();
     }
 })
 
@@ -64,3 +62,10 @@ function chooseColor(vote){
     }
 }
 
+
+function voteCal(vote){    
+    let num = vote;
+    num = num.toString().split('').slice(0,3).join('');
+    num = Number(num);  
+    return num;
+}
